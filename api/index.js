df0691e811fb504e02d5cb58b0325713e5286082
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-19 16:23:06 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./model/userModel");
 const app = express();
 
 // middlewares
@@ -10,5 +12,14 @@ app.get("/", (req, res) => {
   res.json("home route working");
 });
 
+// mongoose connection and listening to port
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, console.log(`server is live:  http://localhost:${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongodb connected");
+    app.listen(PORT, console.log(`server is live:  http://localhost:${PORT}`));
+  })
+  .catch((error) => {
+    console.error("An error occured while connecting", error);
+  });
