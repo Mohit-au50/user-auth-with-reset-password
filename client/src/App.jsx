02bcd06f9020f5cfba7de0fd6126f30ab653b599
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-19 17:06:05 +0530

    update App.jsx, add routes and protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { UserContext } from "./context/userContext";
+// import { toast } from 'react-toastify'
+import axios from "axios";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import NotFound404 from "./pages/NotFound404";
+
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 const App = () => {
-  return <div>App</div>;
+  const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+
+  const getCurrentLoggedInUser = async () => {
+    try {
+      const res = await axios("/current_loggedInUser");
+      console.log(res);
+      if (res.status === 224) {
+        setLoggedInUser(res.data);
+      }
+    } catch (error) {
+      console.error("error on line18", error);
+    }
+  };
+  useEffect(() => {
+    getCurrentLoggedInUser();
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            loggedInUser?.id ? (
+              <Home {...loggedInUser} />
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={loggedInUser?.id ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={loggedInUser?.id ? <Navigate to="/" /> : <Signup />}
+        />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route path="/u/reset_password/:userId" element={<ResetPassword />} />
+        <Route path="/*" element={<NotFound404 />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
