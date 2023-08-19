ef2e8779f0fe66d4eaa0de786c8f849dcea8fc51
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-19 17:25:49 +0530

    add frontend deployed link

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://user-auth-delta.vercel.app/",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
