2f4834251c0a172746b712f50a4290be96b3b480
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-08-20 07:43:44 +0530

    fix clear logout route

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ userName, email, avatar, blur_hash }) => {
 
   const handleLogout = async () => {
     try {
-      const res = await axios("/user/logout");
+      const res = await axios.post("/user/logout");
       console.log(res);
 
       if (res.status === 200) {
